00dfafda2705a09af27b7ad871726c0632396c1a
James Gary <gmarchesi888@gmail.com>
2019-01-06 15:58:13 -0500

    attempting to move database from local to mLab.

--- DB.js
+++ DB.js
@@ -1,1 +1,4 @@
-module.exports = { DB: "mongodb://localhost:27017/react_crud_181228" };
+module.exports = {
+  DB:
+    "mongodb://monster_jg:DZvW9zg9u2iNmH4@ds153700.mlab.com:53700/monsterbase_190106"
+};
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./DB.js");
 const businessRoute = require("./business.route");
+const path = require("path");
 
 mongoose.promise = global.Promise;
 mongoose
@@ -26,7 +27,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/business", businessRoute);
+app.use(express.static(path.join(__dirname, "client", "build")));
 
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname, "client", "build","index.html"));
+})
 app.listen(PORT, function() {
   console.log("Server is running on port ", PORT);
 });
